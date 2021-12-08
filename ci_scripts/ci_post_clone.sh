264461af7ec7dfc51006cef8877e228e66d49223
GoEun <gogo8272@gmail.com>
2021-12-08 09:48:41 +0900

    [Add] ci_post_clone.sh

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  PMS-iOS-V2
+#
+#  Created by GoEun Jeong on 2021/12/08.
+#  
+
+bundle install
+brew install tmspzz/homebrew-tap/rome
+brew tap artemnovichkov/projects
+brew install carting
+brew install xcodegen
+brew tap summerlabs/homebrew-punic
+brew install punic
+chmod +x scripts/run scripts/upload-symbols
+xcodegen
