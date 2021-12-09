0ca6de2d6a519add41b75659fa2472dfb1efdba1
GoEun <gogo8272@gmail.com>
2021-12-09 09:40:45 +0900

    [Remove] Slack notification

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,6 +6,7 @@
 #  Created by GoEun Jeong on 2021/12/08.
 #  
 
+# Install all Dependencies
 bundle install
 export HOMEBREW_NO_INSTALL_CLEANUP=0
 brew install tmspzz/homebrew-tap/rome
@@ -15,6 +16,16 @@ brew install xcodegen
 brew tap summerlabs/homebrew-punic
 brew install punic
 brew install fastlane
+
+# Setting Rome
+echo "[default]" >> .aws/credentials
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> .aws/credentials
+echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> .aws/credentials
+
+echo "[default]" >> .aws/config
+echo "region = $AWS_REGION" >> .aws/config
+
+# Execute Xcodegen
 cd ../
 chmod +x scripts/run scripts/upload-symbols
 xcodegen
